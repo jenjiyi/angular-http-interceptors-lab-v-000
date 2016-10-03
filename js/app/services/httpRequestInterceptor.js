48f56bb664711df1782f3bebbf66395e4fb48e4e
JENNIFER YAMADA <jenny.yamada@gmail.com>
2016-10-03 11:34:40 -0700

    done

--- js/app/services/httpRequestInterceptor.js
+++ js/app/services/httpRequestInterceptor.js
@@ -1,12 +1,12 @@
-function MyInterceptor() {
-    this.responseError = function (config) {
-      alert(config);
+function httpRequestInterceptor() {
+    this.responseError = function () {
+      alert("Error!");
     };
 }
 
 angular
     .module('app')
-    .service('MyInterceptor', MyInterceptor)
+    .service('httpRequestInterceptor', httpRequestInterceptor)
     .config(function ($httpProvider) {
-        $httpProvider.interceptors.push('MyInterceptor');
+        $httpProvider.interceptors.push('httpRequestInterceptor');
     });
